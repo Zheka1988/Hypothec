cd24abe936e6b1f9597bbebb08cbfe580b4a35c6
Zheka1988 <kveladze.kz@mail.ru>
2022-08-03 16:23:55 +0600

    Write acceptance test "User can login in system"   1. install gem Devise   2. create model User   3. write tests model User   4. install gem launchy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'mortgages#index'
+
   resources :mortgages, shallow: true do
     resources :conditions, only: [:create, :update, :destroy]
   end

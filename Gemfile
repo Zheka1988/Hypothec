794ac3d52fcf280f9a15d4b03fc150a1614a802d
Zheka1988 <kveladze.kz@mail.ru>
2022-08-04 17:09:09 +0600

    Install bootstrap   1. change style for page "sign_in, register, forgot_password"   2. change style for header of site

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem "slim-rails"
 gem "devise"
+gem 'bootstrap', '~> 5.1.3'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root to: 'mortgages#index'
   devise_for :users
   
-  # devise_for :users do
-  #   delete'/users/sign_out' => 'devise/sessions#destroy'
-  # end
-
   resources :mortgages, shallow: true do
     resources :conditions, only: [:create, :update, :destroy]
   end
